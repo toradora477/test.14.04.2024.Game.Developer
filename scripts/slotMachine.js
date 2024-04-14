3757055baf6f0f7d7e5f71f3c31de1dfb060b767
Dima Kryshtopa 2 <toradora477@gmail.com>
2024-04-14 23:57:50 +0300

    no message

--- scripts/slotMachine.js
+++ scripts/slotMachine.js
@@ -14,6 +14,63 @@ class SlotMachine {
     this.symbolImages = symbolImages;
     // Вызываем метод для настройки размера канваса
     this.adjustCanvasSize();
+
+    this.adjustButtonPosition();
+  }
+
+  adjustButtonPosition() {
+    const windowHeight = window.innerHeight;
+    const windowWidth = window.innerWidth;
+
+    let marginTopPercentage;
+    if (windowHeight > 1000) {
+      marginTopPercentage = 20;
+    } else {
+      marginTopPercentage = 20;
+    }
+
+    const marginTop = windowHeight * (marginTopPercentage / 100); // Отступ в процентах от высоты окна
+    const marginRight = 20; // Отступ кнопки справа
+    const marginBottom = 20; // Отступ кнопки снизу
+
+    // Применяем отступ к кнопке, устанавливая абсолютное позиционирование и отступы
+    this.spinButton.style.position = 'absolute';
+
+    // const heightMultiplier = 90;
+
+    const widthMultiplier =
+      {
+        [900 >= windowHeight && windowHeight < 1000]: 193, // +
+        [800 >= windowHeight && windowHeight < 900]: 173, // +
+        [700 >= windowHeight && windowHeight < 800]: 173, // +
+        [windowHeight < 700]: 150,
+      }[true] || 215; //* + perfect
+
+    const heightMultiplier =
+      {
+        [900 >= windowHeight && windowHeight < 1000]: 80, // +
+        [800 >= windowHeight && windowHeight < 900]: 70, // +
+        [700 >= windowHeight && windowHeight < 800]: 70, // +
+        [windowHeight < 700]: 63,
+      }[true] || 90; //* + perfect
+
+    const bottomMultiplier =
+      {
+        [900 >= windowHeight && windowHeight < 1000]: -85, // +
+        [800 >= windowHeight && windowHeight < 900]: -75, // +
+        [700 >= windowHeight && windowHeight < 800]: -75, // +
+        [windowHeight < 700]: -70,
+      }[true] || -96; //* + perfect
+
+    this.spinButton.style.bottom = `${bottomMultiplier}px`; //? `-96px`
+    this.spinButton.style.left = `71%`; //* + perfect
+    this.spinButton.style.height = `${heightMultiplier}px`; //? `90px`
+    this.spinButton.style.width = `${widthMultiplier}px`; //? '215px'
+
+    // this.spinButton.style.top = `${marginTop}px`;
+    // this.spinButton.style.bottom = `-96px`;
+    // this.spinButton.style.left = `71%`;
+    // this.spinButton.style.bottom = `${marginBottom}px`;
   }
 
   // Метод для подстройки размеров канваса под размеры окна
@@ -22,11 +79,11 @@ class SlotMachine {
     const heightMultiplier = windowHeight > 1000 ? 0.48 : 0.49; //* + perfect
     const marginTopMultiplier =
       {
-        [900 >= windowHeight && windowHeight < 1000]: 120,
-        [800 >= windowHeight && windowHeight < 900]: 110,
-        [700 >= windowHeight && windowHeight < 800]: 100,
-        [windowHeight < 600]: 95,
-      }[true] || 130; //* + perfect
+        [900 >= windowHeight && windowHeight < 1000]: 74,
+        [800 >= windowHeight && windowHeight < 900]: 65,
+        [700 >= windowHeight && windowHeight < 800]: 56,
+        [windowHeight < 700]: 44,
+      }[true] || 80; //* + perfect
     this.canvas.height = windowHeight * heightMultiplier;
     this.canvas.style.marginTop = `${marginTopMultiplier}px`;
 
